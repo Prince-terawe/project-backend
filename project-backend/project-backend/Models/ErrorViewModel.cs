fd0edb7aa52e86c2d16f34accb1ffc8332d714ca
Prince-terawe <princek@terawe.com>
2025-01-20 12:43:15 +0530

    backend setup complete

---
+++ project-backend/project-backend/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace project_backend.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
